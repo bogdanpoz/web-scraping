a07725292ff6c0203f40afab506f63b11cd136f1
bogdanpoz <bogdanpozdev@gmail.com>
2021-10-18 05:34:16 +0300

    testing of selenium

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -1,7 +1,8 @@
-from selenium.webdriver import Chrome
-driver = Chrome(executable_path='D:/Dev_Tool/ChromeDriver/chromedriver.exe')
-driver.get('https://oxylabs.io/blog')
-blog_titles = driver.get_elements_by_css_selector(' h2.blog-card__content-title')
-for title in blog_tiles:
+import requests
+from bs4 import BeautifulSoup
+url='https://oxylabs.io/blog'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+blog_titles = soup.findAll('h2', attrs={"class":"blog-card__content-title"})
+for title in blog_titles:
     print(title.text)
-driver.quit() # closing the browser
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from selenium.webdriver import Chrome
-driver = Chrome(executable_path='D:/Dev_Tool/ChromeDriver/chromedriver.exe')
+from selenium import webdriver
+driver = webdriver.Chrome()
 driver.get('https://oxylabs.io/blog')
-blog_titles = driver.get_elements_by_css_selector(' h2.blog-card__content-title')
+blog_titles = driver.find_elements_by_css_selector(' h2.blog-card__content-title')
 for title in blog_tiles:
     print(title.text)
 driver.quit() # closing the browser
