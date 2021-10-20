ccc30975c5a011d5d46cce81e0ed6c5ba8a33194
bogdanpoz <bogdanpozdev@gmail.com>
2021-10-20 12:54:04 +0300

    programmatic input and control

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -1,8 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
-url='https://oxylabs.io/blog'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-blog_titles = soup.findAll('h2', attrs={"class":"blog-card__content-title"})
-for title in blog_titles:
-    print(title.text)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+desired_cap = {
+'browserName': 'android',
+'device': 'Samsung Galaxy Note 9',
+'realMobile': 'true',
+'os_version': '8.1',
+'name': 'Bstack-[Python] Sample Test'
+}
+
+driver = webdriver.Remote(
+command_executor='https://<<user>>:<<user.keys>>@hub.browserstack.com:80/wd/hub',
+desired_capabilities=desired_cap)
+
+driver.get("https://www.google.com")
+if not "Google" in driver.title:
+    raise Exception("Unable to load google page!")
+elem = driver.find_element_by_name("q")
+elem.send_keys("BrowserStack")
+elem.submit()
+print (driver.title)
+driver.quit()
